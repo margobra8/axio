9198fd1cd60520a6870d0af5549dc0f4927abcc5
Marcos Gómez <margobra8@gmail.com>
2016-12-20 22:05:27 +0100

    __str__ repr bug fixed, save method override, new objects in methods

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -2,13 +2,14 @@ from django.db import models
 
 
 class AxioURL(models.Model):
-    url = models.CharField(max_length=220, )
+    url = models.CharField(max_length=220)
     shortcode = models.CharField(max_length=15, unique=True)
-    updated = models.DateTimeField(auto_now=True)  # everytime the model is saved
-    timestamp = models.DateTimeField(auto_now_add=True)  # when model was created
+    updated = models.DateTimeField(auto_now=True)  # se genera cada vez que el modelo se guarda
+    timestamp = models.DateTimeField(auto_now_add=True)  # se genera cuando se crea el modelo
 
-    def __str__():
-        return str(self.url)
+    def save(self, *args, **kwargs):
+        print("save method override worked")
+        super(AxioURL, self).save(*args, **kwargs)
 
-    def __unicode__():
+    def __str__(self):
         return str(self.url)
